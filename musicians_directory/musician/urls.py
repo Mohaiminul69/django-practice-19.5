2dc14d3d404677e1cf7d162205ed96149657d2aa
mohaiminul69 <mohaiminul69@gmail.com>
2024-11-29 20:25:06 +0600

    musician class updated to view.

--- musicians_directory/musician/urls.py
+++ musicians_directory/musician/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("add/", views.add_musician, name="add_musician"),
-    path("edit/<int:id>/", views.edit_musician, name="edit_musician"),
-    path("delete/<int:id>/", views.delete_musician, name="delete_musician"),
+    path("add/", views.AddMusicianCreateView.as_view(), name="add_musician"),
+    path("edit/<int:id>/", views.UpdateMusicianView.as_view(), name="edit_musician"),
+    path(
+        "delete/<int:id>/", views.DeleteMusicianView.as_view(), name="delete_musician"
+    ),
 ]
--- musicians_directory/musician/views.py
+++ musicians_directory/musician/views.py
@@ -1,33 +1,35 @@
-from django.shortcuts import render, redirect
 from . import forms
 from . import models
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-def add_musician(request):
-    if request.method == "POST":
-        musician_form = forms.MusicianForm(request.POST)
-        if musician_form.is_valid():
-            musician_form.save()
-            return redirect("add_musician")
-    else:
-        musician_form = forms.MusicianForm()
-    return render(request, "add_musician.html", {"form": musician_form})
+@method_decorator(login_required, name="dispatch")
+class AddMusicianCreateView(CreateView):
+    model = models.Musician
+    form_class = forms.MusicianForm
+    template_name = "add_musician.html"
+    success_url = reverse_lazy("add_musician")
+
+    def form_valid(self, form):
+        return super().form_valid(form)
 
 
-def edit_musician(request, id):
-    musician = models.Musician.objects.get(pk=id)
-    musician_form = forms.MusicianForm(instance=musician)
-    if request.method == "POST":
-        musician_form = forms.MusicianForm(request.POST, instance=musician)
-        if musician_form.is_valid():
-            musician_form.save()
-            return redirect("homepage")
-
-    return render(request, "add_musician.html", {"form": musician_form})
+@method_decorator(login_required, name="dispatch")
+class UpdateMusicianView(UpdateView):
+    model = models.Musician
+    form_class = forms.MusicianForm
+    template_name = "add_musician.html"
+    pk_url_kwarg = "id"
+    success_url = reverse_lazy("homepage")
 
 
-def delete_musician(request, id):
-    musician = models.Musician.objects.get(pk=id)
-    musician.delete()
-    return redirect("homepage")
+@method_decorator(login_required, name="dispatch")
+class DeleteMusicianView(DeleteView):
+    model = models.Musician
+    template_name = "delete_Musician.html"
+    pk_url_kwarg = "id"
+    success_url = reverse_lazy("homepage")
