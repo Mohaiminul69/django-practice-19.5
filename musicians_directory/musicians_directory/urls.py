4732fd21b44641e560d0fbe06915fb7115a05476
mohaiminul69 <mohaiminul69@gmail.com>
2024-11-29 18:39:19 +0600

    basic functionality done.

--- musicians_directory/musicians_directory/urls.py
+++ musicians_directory/musicians_directory/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", home, name="homepage"),
+    path("", views.home, name="homepage"),
+    path("album/", include("album.urls")),
+    path("musician/", include("musician.urls")),
 ]
--- musicians_directory/musicians_directory/views.py
+++ musicians_directory/musicians_directory/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from musician.models import Musician
+from album.models import Album
 
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    musicians = Musician.objects.all()
+    albums = Album.objects.all()
+    return render(request, "home.html", {"musicians": musicians, "albums": albums})
